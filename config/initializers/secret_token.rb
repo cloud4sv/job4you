53e71dd8e1efcc14ec44cab9643ff6f7a5e67d11
Oscar <oskrnav@gmail.com>
2013-06-28 14:50:39 -0600

    Estructura Rails Mysql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Job4you::Application.config.secret_token = '83a59835ef5b7f8192e9a3d5d7c0c9c080f305f067eb649c03d16887b6688274d5037cce7d5bb3039f2d440a2fb0a0b4b181df71bb1b371cfb9f0021f06a5488'
